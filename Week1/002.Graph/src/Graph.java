c41bea8f8de50d766801f03c9b8e74d6c6aab352
Cameron Binns <cambinns123@gmail.com>
2023-02-01 14:03:21 +0100

    Graph met as lijnen Spiral gemaakt

--- Week1/002.Graph/src/Graph.java
+++ Week1/002.Graph/src/Graph.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Graph extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Graph");
@@ -20,6 +21,35 @@ public class Graph extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+        graphics.scale(1,-1);
+
+        graphics.setColor(Color.lightGray);
+        graphics.draw(new Line2D.Double(0, -500, 0, 600));
+        graphics.draw(new Line2D.Double(-1000, 0, 1000, 0));
+
+        for (int x = -1000 ; x < 1000 ; x++) {
+            if (x % 50 == 0) {
+                graphics.draw(new Line2D.Double(x, -10, x, 10));
+            }
+        }
+        for (int y = -500 ; y < 600 ; y++) {
+            if (y % 40 == 0) {
+                graphics.draw(new Line2D.Double(-10, y, 10, y));
+            }
+        }
+
+        double resolution = 0.01;
+        double lastY = Math.pow(-100, 3);
+
+        graphics.setColor(Color.black);
+        for(double x = -100; x < 100; x += resolution)
+        {
+            float y = (float)Math.pow(x, 3);
+            graphics.draw(new Line2D.Double(x * 5 , y * 0.001f, (x-resolution) * 5 , lastY * 0.001f));
+            lastY = y;
+        }
+
     }
     
     
--- Week1/003.Spiral/src/Spiral.java
+++ Week1/003.Spiral/src/Spiral.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Spiral extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Spiral");
@@ -20,6 +21,27 @@ public class Spiral extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+        graphics.scale(1,-1);
+
+        float resolution = 0.01f;
+        float scale = 10;
+        float n = 0.9f;
+        float O = n * 0;
+        float x;
+        float y;
+        float lastX = n * O * (float)Math.cos(O);
+        float lastY = n * O * (float)Math.sin(O);
+
+        for (float r = 0 ; r < 150 ; r += resolution) {
+            O = n * r;
+            x = n * O * (float)Math.cos(O);
+            y = n * O * (float)Math.sin(O);
+            graphics.draw(new Line2D.Float(x*scale, y*scale, lastX*scale, lastY*scale));
+            lastX = x;
+            lastY = y;
+        }
     }
     
     
