48f1d755b1985a92fa4f5023bd96ef880ed2dc63
Cameron Binns <cambinns123@gmail.com>
2023-02-13 13:28:28 +0100

    Colors afgerond

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -16,8 +16,7 @@ public class Colors extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -28,16 +27,43 @@ public class Colors extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
-        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+    public void draw(FXGraphics2D g) {
+        g.setTransform(new AffineTransform());
+        g.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+        g.scale(1, -1);
+        g.setBackground(Color.white);
+        g.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        double x = this.canvas.getWidth();
+        int colour = 0;
+        Color[] colours = new Color[13];
+        colours[0] = Color.BLACK;
+        colours[1] = Color.BLUE;
+        colours[2] = Color.CYAN;
+        colours[3] = Color.DARK_GRAY;
+        colours[4] = Color.GRAY;
+        colours[5] = Color.GREEN;
+        colours[6] = Color.LIGHT_GRAY;
+        colours[7] = Color.MAGENTA;
+        colours[8] = Color.ORANGE;
+        colours[9] = Color.PINK;
+        colours[10] = Color.RED;
+        colours[11] = Color.WHITE;
+        colours[12] = Color.YELLOW;
+
+        for (double i = 0; i <= x; i += (x / 13)) {
+            g.setColor(colours[colour]);
+            g.fill(new Rectangle2D.Double((-x / 2) + i, 0, x/13, x/13));
+//            g.setColor(Color.BLACK);
+//            g.draw(new Rectangle2D.Double((-x / 2) + i, 0, x/13, x/13));
+            colour++;
+        }
+
+
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Colors.class);
     }
 
